2203a99701156cf9fa1acb65ffb89054e4fcff9f
mectos <mectos@outlook.com>
2019-11-23 18:40:44 +0200

    more primary navigation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,16 @@
-const hamburgerIcon = document.getElementById("hamburger");
-const nav = document.getElementById("navigation");
+const header = document.getElementById("js-header");
+const hamburgerIcon = document.getElementById("js-hamburger");
+const nav = document.getElementById("js-navigation");
+const logo = document.getElementById("js-logo");
+
+// Toggle Hamburger icon when open
+function toggleHamburger() {
+	hamburgerIcon.classList.toggle('-open');
+	header.removeChild(hamburgerIcon);
+	header.removeChild(logo);
+	nav.appendChild(hamburgerIcon);
+	nav.appendChild(logo);
+};
 
 // Hamburger icon animation toggle.
 hamburgerIcon.addEventListener("click", () => {
