1130075c342733310ac8b2643bb9fbb92934226e
mectos <mectos@outlook.com>
2019-11-25 20:49:06 +0200

    Remote working 9.1.2019

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,63 +2,63 @@ const sass = require('node-sass');
 
 module.exports = function(grunt){
 
-    //Configuration
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	//Configuration
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-        sass: {
-            dist: {
-                options: {
-                    style: 'expanded'
-                },
-                files: [{
-                    'build/styles/main.css': 'src/styles/main.scss'
-                }]
-            }
-        },
+		sass: {
+			dist: {
+				options: {
+					style: 'expanded'
+				},
+				files: [{
+					'build/styles/main.css': 'src/styles/main.scss'
+				}]
+			}
+		},
 
-        postcss: {
-            options: {
-                map: true,
-                processors: [
-                    require('autoprefixer')({overrideBrowserslist: ['last 4 versions']})
-            ]},
-            dist: {
-                src: 'build/styles/main.css'
-            }
-        },
+		postcss: {
+			options: {
+				map: true,
+				processors: [
+					require('autoprefixer')({overrideBrowserslist: ['last 4 versions']})
+				]},
+			dist: {
+				src: 'build/styles/main.css'
+			}
+		},
 
-        babel: {
-            options: {
-                sourceMap: true,
-                presets: ['@babel/preset-env']
-            },
-            dist: {
-                files: [{
-                'build/scripts/app.min.js': 'src/scripts/app.js'
-                }]
-            }
-        },
+		babel: {
+			options: {
+				sourceMap: true,
+				presets: ['@babel/preset-env']
+			},
+			dist: {
+				files: [{
+					'build/scripts/app.min.js': 'src/scripts/app.js'
+				}]
+			}
+		},
 
-        watch: {
-            css: {
-                files: 'src/styles/**/*.scss',
-                tasks: ['sass', 'postcss']
-            },
-            scripts: {
-                files: ['src/scripts/app.js'],
-                tasks: ['babel']
-            }
-        }
-    });
+		watch: {
+			css: {
+				files: 'src/styles/**/*.scss',
+				tasks: ['sass', 'postcss']
+			},
+			scripts: {
+				files: ['src/scripts/app.js'],
+				tasks: ['babel']
+			}
+		}
+	});
 
-    // Load plugins
+	// Load plugins
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('grunt-postcss');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-babel');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Register tasks
-    grunt.registerTask('default', ['watch']);
+	// Register tasks
+	grunt.registerTask('default', ['watch']);
 };
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,19 +1,67 @@
 const header = document.getElementById("js-header");
 const hamburgerIcon = document.getElementById("js-hamburger");
 const nav = document.getElementById("js-navigation");
-const logo = document.getElementById("js-logo");
+const navHeader = document.getElementById("js-navigation-header");
+
+// Back to top
+const backToTop = document.getElementById("back-to-top");
+backToTop.onclick = () => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+const scroll = () => {
+	if(window.pageYOffset > 330) {
+		backToTop.style.display = "block"
+	} else {
+		backToTop.style.display = "none"
+	}
+}
+
+window.onscroll = () => {
+	scroll()
+}
 
 // Toggle Hamburger icon when open
 function toggleHamburger() {
-	hamburgerIcon.classList.toggle('-open');
-	header.removeChild(hamburgerIcon);
-	header.removeChild(logo);
-	nav.appendChild(hamburgerIcon);
-	nav.appendChild(logo);
-};
+	if (hamburgerIcon.classList.contains("-open")) {
+		hamburgerIcon.classList.toggle("-open");
+		nav.classList.toggle("-open");
+		navHeader.removeChild(hamburgerIcon);
+		header.appendChild(hamburgerIcon);		
+	} else {
+		hamburgerIcon.classList.toggle("-open");
+		nav.classList.toggle("-open");
+		header.removeChild(hamburgerIcon);
+		navHeader.appendChild(hamburgerIcon);
+	}
+}
 
 // Hamburger icon animation toggle.
-hamburgerIcon.addEventListener("click", () => {
-	hamburgerIcon.classList.toggle('-open');
-	nav.classList.toggle('-open');
-}, false);
+hamburgerIcon.addEventListener("click", toggleHamburger, true);
+
+// Carousel
+const tabs = document.querySelectorAll('.carousel__item');
+const tabsTitles = document.querySelectorAll('.carousel__label');
+
+tabsTitles.forEach((tab, index) => {
+	tab.addEventListener("click", () => {
+		tabsTitles.forEach(tab => {
+			tab.classList.remove("-active");
+		});
+		tab.classList.toggle("-active");
+		tabs.forEach(tab => {
+			tab.classList.remove("-active");
+		});
+		tabs[index].classList.toggle("-active");
+	});
+});
+
+// FAQ
+const items = document.querySelectorAll('.faq__item');
+
+items.forEach(function(item) {
+	item.addEventListener('click', function() {
+		item.classList.toggle('-active');
+	}, true);
+});
